758a6b078943f42190b39a241c4411f16865380b
Ntd93 <nguyentiendung2003@gmail.com>
2023-12-31 22:32:40 +0700

    setup project

---
+++ src/main/java/ntd/springboot/blogapprestapi/BlogAppRestApiApplication.java
@@ -0,0 +1,13 @@
+package ntd.springboot.blogapprestapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlogAppRestApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlogAppRestApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ntd/springboot/blogapprestapi/BlogAppRestApiApplicationTests.java
@@ -0,0 +1,13 @@
+package ntd.springboot.blogapprestapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogAppRestApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
